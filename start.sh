b252b7848e1415c435c2af33a357caf95b08c502
alexisbh10 <alexisbh10@gmail.com>
2026-04-24 00:44:48 +0200

    (fix) beta 1.0.1

--- start.sh
+++ start.sh
@@ -2,10 +2,8 @@
 # 1. Registrar comandos y cerrar
 node deploy.js
 
-# 2. Iniciar Lavalink en segundo plano
-java -jar Lavalink.jar &
+# 2. Iniciar Lavalink con un LÍMITE DE RAM (256 MB máximo)
+java -Xmx256m -jar Lavalink.jar &
 
 # 3. Iniciar el bot INMEDIATAMENTE
-# El bot estará en Discord al instante. Shoukaku esperará pacientemente
-# a que Lavalink despierte en el fondo.
 node index.js
