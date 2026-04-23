e3b8829b345927307efc876a04093f177675470b
alexisbh10 <alexisbh10@gmail.com>
2026-04-23 21:44:22 +0200

    (fix) beta 1.0

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# 1. Registrar los comandos en Discord antes de empezar
+node deploy.js
+
+# 2. Iniciar Lavalink en segundo plano
 java -jar Lavalink.jar &
 
+# 3. Esperar a que Lavalink cargue
 sleep 15
 
+# 4. Iniciar el bot
 node index.js
